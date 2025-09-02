a3e56219a037215e683c0dc7f3c65fecc4e2ce4a
marketplace-app123 <info@eventiefiere.com>
2025-09-02 02:20:38 -0700

    Update woocommerce.js

--- src/woocommerce.js
+++ src/woocommerce.js
@@ -1,10 +1,9 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
-
 const api = new WooCommerceRestApi({
-  url: "https://marketplace.eventiefiere.com",
-  consumerKey: "ck_a19ea35df8cc285ac7c867c200c290f16a006a64",
-  consumerSecret: "cs_ff824a43210567a567c22d2dc808f0206bc73965",
-  version: "wc/v3"
+    url: "https://marketplace.eventiefiere.com",
+    consumerKey: "ck_34355d9bdef0c511ff37467233658fb0c1fd7293",
+    consumerSecret: "cs_37ead00d22185f0beb7910dd00831df0818f1337",
+    version: "wc/v3",
+    queryStringAuth: true
 });
-
 export default api;
