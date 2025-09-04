49159690044e3646f3077fe47f85e4c232e25592
marketplace-app123 <info@eventiefiere.com>
2025-09-04 12:14:44 +0000

    config.js pulito

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+// config.js
 export const API_BASE_URL = "https://marketplace.eventiefiere.com/wp-json/wc/v3";
 
 export const API_KEYS = {
-  consumerKey: "ck_34355d9bdef0c511ff37467233658fb0c1fd7293",
-  consumerSecret: "cs_37ead00d22185f0beb7910dd00831df0818f1337"
+  consumerKey: "ck_34355d9bdef0c511ff37467233658fb0c41f67293",
+  consumerSecret: "cs_37ead002d2185f0beb97910ddd08311cf6818f1337",
 };
+
 
